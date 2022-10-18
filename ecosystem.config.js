59c0134f59f9587edd4b0b5260a95bab58addc86
Rauno Tali <rauno@singleton.ee>
2022-10-18 23:12:58 +0300

    add pm2 deploy pipeline

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  deploy: {
+    production: {
+      user: "virt114220",
+      host: ["217.146.69.55"],
+      ref: "origin/main",
+      repo: "git@github.com:raunotal/ProductivityApp.git",
+      ssh_options: ["ForwardAgent=yes"],
+      path: "/data01/virt114220/domeenid/www.retseptipank.ee/htdocs",
+      "pre-deploy-local": "ssh-add",
+      "post-deploy":
+        "yarn && yarn build && pm2 reload /var/www/singleton.ee/current/ecosystem.config.js --env development",
+      env: {
+        PORT: 3000,
+      },
+    },
+  },
+};
