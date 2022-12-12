46883af498a2d4525a4f1b66c321728cfb50bcd4
Rauno Tali <rauno@singleton.ee>
2022-12-12 18:53:56 +0200

    add next-auth dependecies and update prisma dependecies

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,13 @@ import type { GetServerSideProps, NextPage } from "next";
 import GenericLayout from "../components/layout/genericLayout";
 import Main from "../components/section/main";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const todos: ToDo[] = await prisma.toDo.findMany();
+  // const todos: ToDo[] = await prisma.toDo.findMany();
   return {
     props: {
-      todos,
+      todos: [],
     },
   };
 };
