2bc4836619b1e0f3c833e514337afafac13c551a
Rauno Tali <rauno@singleton.ee>
2022-12-16 01:37:59 +0200

    remove npm run start command from pm2 ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       ssh_options: ["ForwardAgent=yes"],
       path: "/data01/virt114220/domeenid/www.retseptipank.ee/deploy/prodApp",
       "post-deploy":
-        "npm install && npm run build && npm run start && pm2 reload /data01/virt114220/domeenid/www.retseptipank.ee/deploy/prodApp/current/ecosystem.config.js",
+        "npm install && npm run build && pm2 reload /data01/virt114220/domeenid/www.retseptipank.ee/deploy/prodApp/current/ecosystem.config.js",
       env: {
         NODE_ENV: "production",
         PORT: 3007,
