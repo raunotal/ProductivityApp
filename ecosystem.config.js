4ef06e25e19439cbcb9e3f65e0d6e4afa8f140b7
Rauno Tali <rauno@singleton.ee>
2022-12-16 01:04:13 +0200

    add npm build command to post-deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       path: "/data01/virt114220/domeenid/www.retseptipank.ee/deploy/prodApp",
       "pre-deploy-local": "ssh-add",
       "post-deploy":
-        "npm --name prodApp && npm start && pm2 reload /data01/virt114220/domeenid/www.retseptipank.ee/deploy/prodApp/current/ecosystem.config.js",
+        "npm --name prodApp && npm build && npm start && pm2 reload /data01/virt114220/domeenid/www.retseptipank.ee/deploy/prodApp/current/ecosystem.config.js",
       env: {
         NODE_ENV: "production",
       },
